c141e64e4d92d5e7f576f44fd5fae4679d32f52b
Mateusz Kowalski <80004522+matiszach@users.noreply.github.com>
2024-04-05 14:55:49 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+from datetime import date, timedelta
+import lib.scraper as scraper
+import lib.manipulation as manip
+import lib.gpw as gpw
+import lib.notifications as mailing
+import math
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+min_volume = int(os.getenv("MIN_VOLUME"))
+
+pre, curr, tickers = gpw.init()
+
+tickers = gpw.filter_data(curr, tickers, min_volume)
+tickers = gpw.sort_by_volume(curr, tickers)
+tickers = gpw.get_inside_bars(pre, curr, tickers)
+
+df = gpw.get_inside_bars_strategy(pre, curr, tickers)
+files = mailing.make_table(df)
+
+
+content = ("W tym tygodniu inside bar wystąpił na " + str(len(tickers))
+           + " spółkach z obrotem co najmniej " + str(min_volume) + "k PLN.")
+
+mailing.send_mail(mailing.make_title('Wybicia tygodnia '), content, files, os.getenv('SENDER'),
+                  os.getenv('RECIPIENT'), os.getenv('PASSWORD'))
